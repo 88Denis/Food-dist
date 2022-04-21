778a983bd939b7e7dea20b92308a6f66fff67327
Denis <denis-serov00@mail.ru>
2022-04-21 22:43:44 +0600

    Модификации модального окна

--- js/script.js
+++ js/script.js
@@ -97,29 +97,27 @@ window.addEventListener("DOMContentLoaded", () => {
         modal = document.querySelector('.modal');
         modalCloseBtn = document.querySelector('[data-close]');
 
+  function openModal() {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+      }
+      
 
+      
   modalTrigger.forEach(btn => {
-    
-    btn.addEventListener('click', () => {
-      // modal.classList.add('show');
-      // modal.classList.remove('hide');
-      modal.classList.toggle('show');
-      document.body.style.overflow = 'hidden';
-    });
+    btn.addEventListener('click', openModal);
   });
 
-function closeModal() {
-      // modal.classList.add('hide');
-    // modal.classList.remove('show');
-    modal.classList.toggle('show');
-    document.body.style.overflow = '';
-}
+  function closeModal() {
+      modal.classList.add('hide');
+      modal.classList.remove('show');
+      document.body.style.overflow = '';
+  }
 
   modalCloseBtn.addEventListener('click', closeModal);
         
-        
-
-
   modal.addEventListener('click', (e) => {
     if (e.target === modal) {
       closeModal();
@@ -132,6 +130,16 @@ function closeModal() {
     }
   });
 
+
+  const modalTimerId = setTimeout(openModal, 3000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.   documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    } 
+  }
+  window.addEventListener('scroll', showModalByScroll);
 });
 
 
